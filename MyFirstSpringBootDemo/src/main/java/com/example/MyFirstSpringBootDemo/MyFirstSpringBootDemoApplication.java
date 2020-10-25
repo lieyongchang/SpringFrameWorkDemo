048f3bcc822639c0b9b9a457944b346ff8e0fb95
lieyongchang <lieyongchang@gmail.com>
2020-10-25 17:51:12 +0800

    Clean project

---
+++ MyFirstSpringBootDemo/src/main/java/com/example/MyFirstSpringBootDemo/MyFirstSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstSpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootDemoApplication.class, args);
+	}
+
+}
