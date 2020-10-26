dc770bf2d085a639f61ab64034e068af1c46dd29
lieyongchang <lieyongchang@gmail.com>
2020-10-26 18:47:22 +0800

    Recipe web app

---
+++ RecipeWebApp/src/test/java/com/example/RecipeWebApp/RecipeWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecipeWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
