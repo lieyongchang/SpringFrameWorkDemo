72b118c4ef485eb1116b1dc53f972c7baa07c4bb
lieyongchang <lieyongchang@gmail.com>
2020-10-27 10:51:44 +0800

    Only have the basic classes added

---
+++ RecipeWebApp/src/main/java/domain/Recipe.java
@@ -0,0 +1,136 @@
+package domain;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+//defines that a class can be mapped to a table
+@Entity
+public class Recipe {
+
+	/*
+	 * @Id = the member field below is the primary key of current entity.
+	 * 
+	 * 
+	 * @GeneratedValue = configure the way of increment of the specified
+	 * column(field)
+	 * 
+	 */
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String description;
+	private Integer prepTime;
+	private Integer cookTime;
+	private Integer serving;
+	private String source;
+	private String url;
+	private String directions;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients;
+
+	@Lob
+	private Byte[] image;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private Notes notes;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getPrepTime() {
+		return prepTime;
+	}
+
+	public void setPrepTime(Integer prepTime) {
+		this.prepTime = prepTime;
+	}
+
+	public Integer getCookTime() {
+		return cookTime;
+	}
+
+	public void setCookTime(Integer cookTime) {
+		this.cookTime = cookTime;
+	}
+
+	public Integer getServing() {
+		return serving;
+	}
+
+	public void setServing(Integer serving) {
+		this.serving = serving;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDirections() {
+		return directions;
+	}
+
+	public void setDirections(String directions) {
+		this.directions = directions;
+	}
+
+	public Byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(Byte[] image) {
+		this.image = image;
+	}
+
+	public Notes getNotes() {
+		return notes;
+	}
+
+	public void setNotes(Notes notes) {
+		this.notes = notes;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+}
