617a6cd90e13651ea86d4603abc49c855a42d342
lieyongchang <lieyongchang@gmail.com>
2020-10-28 17:14:42 +0800

    Implement stuff to test Spring Frame work application

---
+++ RecipeWebApp/src/main/java/com/example/RecipeWebApp/controllers/IndexController.java
@@ -0,0 +1,53 @@
+package com.example.RecipeWebApp.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.RecipeWebApp.Service.IRecipeService;
+@Slf4j
+@Controller
+public class IndexController {
+
+	private final IRecipeService recipeService;
+
+	public IndexController(IRecipeService recipeService) {
+		log.debug("this is index page");
+		this.recipeService = recipeService;
+	}
+
+	/*
+	 * 
+	 * Uncomment this to see JPA Query in action
+	 * 
+	 */
+	// private CategoryRepository categoryRepo;
+	// private UnitOfMeasureRepository uomRepo;
+
+	// public IndexController(CategoryRepository categoryRepo,
+	// UnitOfMeasureRepository uomRepo) {
+	// super();
+	// this.categoryRepo = categoryRepo;
+	// this.uomRepo = uomRepo;
+	// }
+
+	// @RequestMapping(value = "/")
+	@RequestMapping({ "", "/", "/index" })
+	public String getIndexPage(Model model) {
+		model.addAttribute("recipes", recipeService.getRecipe());
+		/*
+		 * 
+		 * Uncomment this to see JPA Query in action
+		 * 
+		 */
+
+		// Optional<Category> categoryOptional =
+		// categoryRepo.findByDescription("American");
+		// Optional<UnitOfMeasure> uomOptional = uomRepo.findByDescription("Teaspoon");
+		//
+		// System.out.println("category: " + categoryOptional.get().getId());
+		// System.out.println("Unit of Measure: " + uomOptional.get().getId());
+		return "Index";
+	}
+}
