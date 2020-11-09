3fabecbd338719a2b4c500935303ef5ebf9b3bb9
lieyongchang <lieyongchang@gmail.com>
2020-11-09 10:11:07 +0800

    MongoDB with SpringFrameWork

--- spring5-mongo-recipe-app/src/main/java/guru/springframework/Spring5MongoRecipeAppApplication.java
+++ spring5-mongo-recipe-app/src/main/java/guru/springframework/Spring5MongoRecipeAppApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5MongoRecipeAppApplication {
-
+	/*
+	* This is about mongoDB with SpringFrameWork
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5MongoRecipeAppApplication.class, args);
 	}
