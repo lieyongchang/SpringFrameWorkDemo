46069d53a22cb66961b2c0e738a9b444fcf83979
lieyongchang <lieyongchang@gmail.com>
2020-11-11 14:46:47 +0800

    # very simple demo of RESTful api service

---
+++ restful_webapi_tutorialpoint/src/test/java/com/example/restful_webapi_tutorialpoint/RestfulWebapiTutorialpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restful_webapi_tutorialpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebapiTutorialpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
