973ed289bb9e28dce895d564a123ef1f3dfa3806
lieyongchang <lieyongchang@gmail.com>
2020-11-11 16:42:31 +0800

    # Post URL is broken

--- restful_webservice/src/main/java/com/restful_webservice/services/ApiServiceImpl.java
+++ restful_webservice/src/main/java/com/restful_webservice/services/ApiServiceImpl.java
@@ -2,25 +2,33 @@ package com.restful_webservice.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.api.domain.User;
 import com.api.domain.UserData;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class ApiServiceImpl implements ApiService {
 
 	private RestTemplate restTemplate;
+	private final String api_url;
 
-	public ApiServiceImpl(RestTemplate restTemplate) {
+	public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
 		this.restTemplate = restTemplate;
+		this.api_url = api_url;
 	}
 
 	@Override
 	public List<User> getUsers(Integer limit) {
-		List<User> userData = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users?limit=" + limit, List.class);
-		return userData;
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder
+				.fromUriString(api_url)
+				.queryParam("limit", limit);
+
+		UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
+		return userData.getData();
 	}
 
 }
