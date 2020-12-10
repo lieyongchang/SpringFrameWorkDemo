aa1c3ecad212390defe8d847637bcec73f8a564d
lieyongchang <lieyongchang@gmail.com>
2020-12-10 18:04:46 +0800

    # submit form as long as the contact field matches with the email/Mobile

--- src/main/java/Validator/UserValidator.java
+++ src/main/java/Validator/UserValidator.java
@@ -16,15 +16,20 @@ public class UserValidator implements Validator {
 	public void validate(Object target, Errors errors) {
 		User user = (User) target;
 
-		//@formatter:off
-		if (user.getContact() == user.getEmail() || 
-		    user.getContact() == Integer.toString(user.getMobile())
-		   ) 
-		{
-
-			errors.rejectValue("name", "your_error_code");
+		if (user.getContact().equals("0")) {
+			return;
 		}
 
+		//@formatter:off
+		if ((
+			user.getContact().equals(user.getEmail()) || 
+			user.getContact().equals(Integer.toString(user.getMobile()))
+		    )) 
+		{
+			System.out.println("true");
+		} else {
+			errors.rejectValue("name", "your_error_code");
+		}
 	}
 
 }
--- src/main/java/com/controller/WelcomeController.java
+++ src/main/java/com/controller/WelcomeController.java
@@ -62,6 +62,13 @@ class WelcomeController {
 	public String submitForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
 //		System.out.println(user);
 
+		if (user.getContact().equalsIgnoreCase("Email")) {
+			user.setContact(user.getEmail());
+		} else if (user.getContact().equalsIgnoreCase("Mobile") && user.getMobile() != 0) {
+			System.out.print("enter here");
+			user.setContact(Integer.toString(user.getMobile()));
+		}
+
 		UserValidator userValidator = new UserValidator();
 		userValidator.validate(user, bindingResult);
 
