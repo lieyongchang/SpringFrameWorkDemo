5572c69f709b03dd0430979a9f06f81ee442c975
lieyongchang <lieyongchang@gmail.com>
2020-12-11 18:30:10 +0800

    # Able to open update user page

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -3,7 +3,6 @@ package com.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -46,15 +45,16 @@ public class UserController {
 	 * @param model
 	 * @return
 	 */
-	/*
-	 * @GetMapping("/edit/{id}") public String showUpdateForm(@PathVariable("id")
-	 * long id, Model model) { User user = userRepository.findById(id)
-	 * .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-	 * 
-	 * model.addAttribute("user", user); return "update-user"; }
-	 */
-	/*
-	 * */
+
+	@RequestMapping("/edit/{id}")
+	public String showUpdateForm(@PathVariable("id") long id, Model model) {
+		User user = userRepository.findById(id)
+		        .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+		model.addAttribute("user", user);
+		return "/user/update-user";
+	}
+
 	/**
 	 * Update user
 	 * 
@@ -81,7 +81,7 @@ public class UserController {
 	 * @return
 	 */
 
-	@GetMapping("/delete/{id}")
+	@RequestMapping("/delete/{id}")
 	public String deleteUser(@PathVariable("id") long id, Model model) {
 		User user = userRepository.findById(id)
 		        .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
